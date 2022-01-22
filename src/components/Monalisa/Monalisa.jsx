e66b5174d1dc8fb81f9ee9e5e81cdad069c81908
vinciis2018 <vinciis2018@gmail.com>
2022-01-22 20:41:03 +0530

    1st commit

---
+++ src/components/Monalisa/Monalisa.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import axios from 'axios';
+import '../../styles.css';
+
+export const Monalisa = ({props}) => {
+  const screenId = props;
+  // const screenId = "61e1532b4cdb8cfa375286da";
+  const [index, setIndex] = React.useState(0);
+  const delay = 5000;
+  const timeoutRef = React.useRef(null);
+  const [videos, setVideos] = React.useState([]);
+
+  function resetTimeout() {
+    if(timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
+
+  React.useEffect(async () => {
+    await axios.get(`https://vblinds.herokuapp.com/api/screens/${screenId}/screenVideos`)
+      .then((res) => {
+        setVideos(res.data);
+        console.log(res.data)
+      }).then(() => {
+        resetTimeout();
+        timeoutRef.current = setTimeout(
+          () => 
+            setIndex((prevIndex) => 
+              prevIndex === videos.length - 1 ? 0 : prevIndex + 1
+            ),
+            delay
+        );
+      })
+    return () => {
+      resetTimeout();
+    };
+  }, [
+    index, 
+    videos
+  ]);
+
+  return (
+    <div>
+      <div className="">
+        <div className="slideshow">
+          <div 
+            className="slideshowSlider" 
+            style={{transform: `translate3d(${-index * 100}%, 0, 0)`}}
+            >
+              {videos ? videos.map((video, index) => (
+                <img 
+                className="slide"
+                key={index}
+                src={video.thumbnail}
+                alt={video.name}
+                >
+                </img>
+              )) : (
+              <h3>Monalisa By Vinciis</h3>
+              )}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
